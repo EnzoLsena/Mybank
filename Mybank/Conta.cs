bf09dde341f0f968ff863ebc9fb44c6b2ac9b205
EnzoLsena <enzolevysena@gmail.com>
2024-08-30 12:57:10 -0300

    GG

---
+++ Mybank/Conta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Mybank {
+	internal class Conta {
+		public double Saldo;
+
+		public override string ToString() {
+			return $"Saldo Atual: R$ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+		}
+
+		public void Adcsaldo(double saldo) {
+			Saldo += saldo;
+
+		}
+
+		public void removersaldo(double saldo) {
+			Saldo -= saldo;
+
+		}
+	}
+}
---
+++ Mybank/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+namespace Mybank {
+	class Program {
+		static void Main(string[] args) {
+			Conta conta = new Conta();
+			conta.Saldo = 500.00;
+
+
+			while (conta.Saldo > 0) {
+				
+				Console.WriteLine("******************************************");
+				Console.WriteLine(" ");
+				Console.WriteLine("Bem-vind@ ao FranceBank!");
+				Console.WriteLine(" ");
+				Console.WriteLine("Menu de Opções: ");
+				Console.WriteLine("° 1 Depósitos ");
+				Console.WriteLine("° 2 Saques ");
+				Console.WriteLine("° 3 Consulta de Saldo ");
+				Console.WriteLine(" ");
+				Console.Write("Digite a opção desejada: ");
+				int option = int.Parse(Console.ReadLine());
+				Console.WriteLine(" ");
+				Console.WriteLine("*******************************************");				
+				Console.WriteLine(" ");
+				double qte;
+				if (option == 1) {
+					Console.Write("Digite a quantia que deseja depositar: ");
+					qte = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+					conta.Adcsaldo(qte);
+					Console.WriteLine(" ");
+					Console.WriteLine("Depósito efetuado com sucesso!");
+                    
+					Console.WriteLine(" ");
+
+				} else if (option == 2) {
+					Console.Write("Digite a quantia que deseja sacar: ");
+					qte = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+					conta.removersaldo(qte);
+					Console.WriteLine(" ");
+					Console.WriteLine("Saque realizado com sucesso!");
+
+                    Console.WriteLine(" ");
+
+				} else if (option == 3) {
+						Console.WriteLine(conta);
+						Console.WriteLine(" ");
+
+				} else {
+                    Console.WriteLine("Opção invalida!");
+                    Console.WriteLine(" ");
+					Console.WriteLine(" para continuar digite 1, 2 ou 3");
+					Console.WriteLine(" ");
+				}
+
+			}
+			Console.WriteLine("FranceBank, agradece a sua preferencia!");
+
+
+		}
+	}
+}
+
+
+
